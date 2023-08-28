c7ddbc7a6a4b0b52eb1c8b75e4e6d3da428e9450
Nidhi857 <nidhi@parrophins.com>
2023-08-28 13:32:37 +0530

    Update feeReceiptPrint.php

--- STAFF_PORTAL/application/views/fees/feeReceiptPrint.php
+++ STAFF_PORTAL/application/views/fees/feeReceiptPrint.php
@@ -45,6 +45,98 @@ table{
 
 
 <!-- RECEIPT 2 -->
+<div style="text-align:right"><span style="font-size: 13px;">STUDENT COPY</span></div>
+<div class="container-fluid border_full">
+    <div class="row">
+        <div class="">
+            <table class="table text_highlight">
+                <tr>
+                    <td style="text-align:center;" width="80">
+                        <img class="mt-2" width="50" height="50" src="<?php echo INSTITUTION_LOGO; ?>" alt="logo">
+                    </td>
+                    <td width="200" style="text-align:center;">
+                        <b style="font-size: 12px;margin-bottom: 2px;"><?php echo TITLE; ?></b><br/>
+                        <!-- <b style="font-size: 13px;margin-bottom: 2px;">Unit of KJES </b><br/>
+                        <b style="font-size: 12px;margin-bottom: 2px;"># 23, Vittal Mallya Road, Bengaluru - 560001</b><br/> -->
+                        <!-- <b style="font-size: 12px;margin-bottom: 2px;">Bengaluru – 560 001</b><br/> -->
+                    </td>
+                </tr>
+            </table>
+            <hr class="border_bottom hr_line">
+            <table class="table" style="font-size: 10px;">
+                <tr>
+                    <td>Name of the child : <?php echo strtoupper($studentInfo->student_name); ?></td>
+                    <td>Application/Register no. : <?php if(!empty($studentInfo->register_number) && $studentInfo->term_name == 'II PUC'){ echo $studentInfo->register_number; }else{ echo $studentInfo->application_no; } ?></td>
+                </tr>
+                <tr>
+                    <!-- <td>Name of the father : <?php echo $studentInfo->father_name; ?></td> -->
+                    <td>Receipt no.: <?php echo $feeInfo->ref_receipt_no; ?></td>
+                    <td>Payment Received Date : <?php echo date('d-m-Y',strtotime($feeInfo->payment_date)); ?></td>
+                </tr>
+                <tr>
+      
+                    <!-- <td>Transaction Id : <?php echo $feeInfo->order_id; ?></td> -->
+                </tr>
+                <tr>
+                    <td width="300">Class & Section : <?php echo strtoupper($studentInfo->term_name.' '.$studentInfo->section_name); ?></td>
+                    <td >Payment Received Mode : <?php if(!empty($feeInfo->payment_type)){ echo $feeInfo->payment_type; }else{ echo 'Online'; } ?></td>
+                </tr>
+            </table>
+            <table class="table table_bordered" style="font-size: 9px;">
+                <tr>
+                    <!-- <th width="100">Sl.No.</th> -->
+                    <th>Particulars</th>
+                    <th>Amount</th>
+                </tr> 
+                <?php if(!empty($feeStructureInfo)) {
+                    $i=1; $total_fee_amt=0;
+                    foreach($feeStructureInfo as $fee){ 
+                        if($fee->fees_type != 'College Dept Fee' && $fee->fees_type != 'Eligibility Fee'){
+                        $total_fee_amt +=  $fee->fee_amount_state_board;
+                ?>
+                     <tr>
+                        <!-- <th style="text-align:center;"><?php echo $i; ?></th> -->
+                        <th style="text-align: center;"><?php echo strtoupper($fee->fees_type); ?></th>
+                        <th style="text-align: right;"><?php echo number_format($fee->fee_amount_state_board,2); ?></th>
+                    </tr>
+                   
+                <?php $i++; }else{
+                    $dept_fee = $fee->fee_amount_state_board;
+                } } } ?>  
+
+                <tr>
+                    <th colspan="1">Total Fee</th>
+                    <th class="border_right_none" style="text-align: right;"><?php if($feeInfo->attempt == "1"){ echo number_format($total_fee_amt - 2000,2);} else {
+                    echo number_format($total_fee_amt,2);
+                    }; ?></th>
+                </tr>
+                <?php if($fee_concession != 0){ ?>
+                <tr>
+                    <th colspan="1">Fee Concession(-)</th>
+                    <th class="border_right_none" style="text-align: right;"><?php echo number_format($fee_concession,2); ?></th>
+                </tr>
+                <?php } ?>
+                <tr>
+                    <th colspan="1">Amount Paid</th>
+                    <th class="border_right_none" style="text-align: right;"><?php if($feeInfo->payment_count == 1){ $paidAmt = $feeInfo->paid_amount; }else{ $paidAmt =$feeInfo->paid_amount; } echo number_format($paidAmt,2); ?></th>
+                </tr>
+                <tr>
+                    <th colspan="1">Amount Pending</th>
+                    <th class="border_right_none" style="text-align: right;"><?php echo number_format($feeInfo->pending_balance,2); ?></th>
+                </tr>
+                <tr>
+                    <td colspan="2"><b>Paid total amount in word: <span style="text-transform: capitalize;"><?php echo strtoupper(getIndianCurrency($feeInfo->paid_amount)).' ONLY'; ?></span></b></td>
+                </tr>   
+            </table>
+
+        </div>
+    </div>
+  
+</div>
+<b style="font-size: 8px;">All fees paid are not transferable or refundable.</b>
+<b style="font-size: 8px;">This is an online generated fee Receipt no seal and signature is required.</b>
+<br><br><br><br><br><br><br><br><br><br>
+<div style="text-align:right"><span style="font-size: 13px;">OFFICE COPY</span></div>
 <div class="container-fluid border_full">
     <div class="row">
         <div class="">
