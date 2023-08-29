7107a5b76a22aa923896d7e067f467e8abc9f4a8
Nidhi857 <nidhi@parrophins.com>
2023-08-29 22:51:17 +0530

    receipt changes

--- STAFF_PORTAL/application/controllers/Fee.php
+++ STAFF_PORTAL/application/controllers/Fee.php
@@ -1070,14 +1070,61 @@ class Fee extends BaseController
         $data['studentInfo'] = $studentInfo;
         
         $this->global['pageTitle'] = ''.TAB_TITLE.' : Fee Receipt';
-        $mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir().DIRECTORY_SEPARATOR.'mpdf','default_font' => 'timesnewroman', 'format' => [210, 297]]);
-        $mpdf->AddPage('P','','','','',7,7,7,7,8,8);
-        $mpdf->SetTitle('Fee Receipt');
-        $html = $this->load->view('fees/feeReceiptPrint',$data,true);
-        $mpdf->WriteHTML($html);
+        $mpdf = new \Mpdf\Mpdf(['tempDir' => sys_get_temp_dir().DIRECTORY_SEPARATOR.'mpdf','default_font' => 'timesnewroman', 'format' => 'A4-L']);
+        $mpdf->AddPage('L','','','','',10,10,15,15,8,8);
+        // $mpdf->SetTitle('Fee Receipt');
+        // $html = $this->load->view('fees/feeReceiptPrint',$data,true);
+        // $mpdf->WriteHTML($html);
+        $data['paid_amount'] = $data['feeInfo']->paid_amount;
+        $data['paid_amount_words'] = $this->getIndianCurrency(floatval($data['paid_amount']));
+        $data['name_count'] = 0;
+        $html_student_copy = $this->load->view('fees/feeReceiptPrint',$data,true);
+        $data['name_count'] = 1;
+        $html_office_copy = $this->load->view('fees/feeReceiptPrint',$data,true);
+       
+        
+        $mpdf->WriteHTML('<columns column-count="2" vAlign="J" column-gap="10" />');
+        $mpdf->WriteHTML($html_student_copy);
+        $mpdf->WriteHTML($html_office_copy);
+       
+        
+
         $mpdf->Output('Fee_Receipt.pdf', 'I');
         
     }
+
+    function getIndianCurrency(float $number) {
+            $decimal = round($number - ($no = floor($number)), 2) * 100;
+            $hundred = null;
+            $digits_length = strlen($no);
+            $i = 0;
+            $str = array();
+            $words = array(0 => '', 1 => 'one', 2 => 'two',
+                3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six',
+                7 => 'seven', 8 => 'eight', 9 => 'nine',
+                10 => 'ten', 11 => 'eleven', 12 => 'twelve',
+                13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen',
+                16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen',
+                19 => 'nineteen', 20 => 'twenty', 30 => 'thirty',
+                40 => 'forty', 50 => 'fifty', 60 => 'sixty',
+                70 => 'seventy', 80 => 'eighty', 90 => 'ninety');
+            $digits = array('', 'hundred','thousand','lakh', 'crore');
+            while( $i < $digits_length ) {
+                $divider = ($i == 2) ? 10 : 100;
+                $number = floor($no % $divider);
+                $no = floor($no / $divider);
+                $i += $divider == 10 ? 1 : 2;
+                if ($number) {
+                    $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
+                    $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+                    $str [] = ($number < 21) ? $words[$number].' '. $digits[$counter]. $plural.' '.$hundred:$words[floor($number / 10) * 10].' '.$words[$number % 10]. ' '.$digits[$counter].$plural.' '.$hundred;
+                } else $str[] = null;
+            }
+            $Rupees = implode('', array_reverse($str));
+            $paise = ($decimal > 0) ? "." . ($words[$decimal / 10] . " " . $words[$decimal % 10]) . ' Paise' : '';
+            return ($Rupees ? $Rupees . 'Rupees ' : '') . $paise;
+        }
+
     public function addBankSettlementSubmitNewAdm(){
         if($this->isAdmin() == TRUE){
             $this->loadThis();
--- STAFF_PORTAL/application/views/fees/feeReceiptPrint.php
+++ STAFF_PORTAL/application/views/fees/feeReceiptPrint.php
@@ -16,6 +16,8 @@ table{
 }*/
 .border_full{
     border: 1px solid black;
+    height: 100% !important;
+    
     /* height: 90% !important; */
 }
 .border_bottom{
@@ -29,24 +31,32 @@ table{
 .table_bordered{
     border-collapse: collapse;
 }
-.table_bordered th,.table_bordered td{
+.table_bordered th,{
     border-top: 1px solid black;
     border-right: 1px solid black;
     padding: 3px;
+}
+.table_bordered td{
+    border-top: 1px solid black;
+   
+    
 }
 
 .table_bordered th .border_right_none,.table_bordered td .border_right_none{
     border-right: 1px solid transparent !important;
 }
 
+.centered-td {
+        text-align: center !important;
+    }
+
 </style>
 
 
 <!-- RECEIPT 2 -->
 <?php 
-        $copy_name = ['STUDENT COPY','COLLEGE COPY'] ?>
-        <?php for($r=0;$r<2;$r++){ ?>
-
+        $copy_name = ['STUDENT COPY','OFFICE COPY'] ?>
+        <?php //for($r=0;$r<2;$r++){ ?>
 <div class="container-fluid border_full">
     <div class="row">
     
@@ -61,7 +71,7 @@ table{
                     
                     <td width="200" style="text-align:center;">
                     
-                        <b style="font-size: 12px;margin-bottom: 2px;"><?php echo TITLE; ?></b><br/>
+                        <b style="font-size: 13px;margin-bottom: 1px;"><?php echo TITLE; ?></b><br/>
                         <!-- <b style="font-size: 13px;margin-bottom: 2px;">Unit of KJES </b><br/>
                         <b style="font-size: 12px;margin-bottom: 2px;"># 23, Vittal Mallya Road, Bengaluru - 560001</b><br/> -->
                         <!-- <b style="font-size: 12px;margin-bottom: 2px;">Bengaluru – 560 001</b><br/> -->
@@ -69,7 +79,12 @@ table{
                 </tr>
             </table>
             <hr class="border_bottom hr_line">
-            <table class="table" style="font-size: 10px;">
+            <table class="table" style="font-size: 13px;">
+                <tr>
+                    <td class="centered-td">Fee Receipt (<?php echo $copy_name[$name_count]; ?>)</td>
+                </tr>
+                </table>
+                <table class="table" style="font-size: 13px;">
                 <tr>
                     <td width="50%">Name of the child : <?php echo strtoupper($studentInfo->student_name); ?></td>
                     <td>Application/Register no. : <?php if(!empty($studentInfo->register_number) && $studentInfo->term_name == 'II PUC'){ echo $studentInfo->register_number; }else{ echo $studentInfo->application_no; } ?></td>
@@ -88,7 +103,7 @@ table{
                     <td >Payment Received Mode : <?php if(!empty($feeInfo->payment_type)){ echo $feeInfo->payment_type; }else{ echo 'Online'; } ?></td>
                 </tr>
             </table>
-            <table class="table table_bordered" style="font-size: 8px;">
+            <table class="table table_bordered" style="font-size: 13px;">
                 <tr>
                     <!-- <th width="100">Sl.No.</th> -->
                     <th>Particulars</th>
@@ -102,7 +117,7 @@ table{
                 ?>
                      <tr>
                         <!-- <th style="text-align:center;"><?php echo $i; ?></th> -->
-                        <th style="text-align: center;"><?php echo strtoupper($fee->fees_type); ?></th>
+                        <th style="text-align: left;"><?php echo strtoupper($fee->fees_type); ?></th>
                         <th style="text-align: right;"><?php echo number_format($fee->fee_amount_state_board,2); ?></th>
                     </tr>
                    
@@ -111,27 +126,27 @@ table{
                 } } } ?>  
 
                 <tr>
-                    <th colspan="1">Total Fee</th>
+                    <th style="text-align: left;" colspan="1">Total Fee</th>
                     <th class="border_right_none" style="text-align: right;"><?php if($feeInfo->attempt == "1"){ echo number_format($total_fee_amt - 2000,2);} else {
                     echo number_format($total_fee_amt,2);
                     }; ?></th>
                 </tr>
                 <?php if($fee_concession != 0){ ?>
                 <tr>
-                    <th colspan="1">Fee Concession(-)</th>
+                    <th style="text-align: left;" colspan="1">Fee Concession(-)</th>
                     <th class="border_right_none" style="text-align: right;"><?php echo number_format($fee_concession,2); ?></th>
                 </tr>
                 <?php } ?>
                 <tr>
-                    <th colspan="1">Amount Paid</th>
+                    <th style="text-align: left;" colspan="1">Amount Paid</th>
                     <th class="border_right_none" style="text-align: right;"><?php if($feeInfo->payment_count == 1){ $paidAmt = $feeInfo->paid_amount; }else{ $paidAmt =$feeInfo->paid_amount; } echo number_format($paidAmt,2); ?></th>
                 </tr>
                 <tr>
-                    <th colspan="1">Amount Pending</th>
+                    <th style="text-align: left;" colspan="1">Amount Pending</th>
                     <th class="border_right_none" style="text-align: right;"><?php echo number_format($feeInfo->pending_balance,2); ?></th>
                 </tr>
                 <tr>
-                    <td colspan="2"><b>Paid total amount in word: <span style="text-transform: capitalize;"><?php echo strtoupper(getIndianCurrency($feeInfo->paid_amount)).' ONLY'; ?></span></b></td>
+                    <td colspan="2"><b>Paid total amount in word: <span style="text-transform: capitalize;"><?php echo $paid_amount_words.' ONLY'; ?></span></b></td>
                 </tr>   
             </table>
 
@@ -139,43 +154,43 @@ table{
     </div>
     </div>
 
-<b style="font-size: 8px;">All fees paid are not transferable or refundable.</b>
-<b style="font-size: 8px;">This is an online generated fee Receipt no seal and signature is required.</b>
-<br><br>
-<?php } ?>
+
+
+
+<?php //} ?>
 
 
 <?php 
-function getIndianCurrency(float $number) {
-    $decimal = round($number - ($no = floor($number)), 2) * 100;
-    $hundred = null;
-    $digits_length = strlen($no);
-    $i = 0;
-    $str = array();
-    $words = array(0 => '', 1 => 'one', 2 => 'two',
-        3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six',
-        7 => 'seven', 8 => 'eight', 9 => 'nine',
-        10 => 'ten', 11 => 'eleven', 12 => 'twelve',
-        13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen',
-        16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen',
-        19 => 'nineteen', 20 => 'twenty', 30 => 'thirty',
-        40 => 'forty', 50 => 'fifty', 60 => 'sixty',
-        70 => 'seventy', 80 => 'eighty', 90 => 'ninety');
-    $digits = array('', 'hundred','thousand','lakh', 'crore');
-    while( $i < $digits_length ) {
-        $divider = ($i == 2) ? 10 : 100;
-        $number = floor($no % $divider);
-        $no = floor($no / $divider);
-        $i += $divider == 10 ? 1 : 2;
-        if ($number) {
-            $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
-            $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
-            $str [] = ($number < 21) ? $words[$number].' '. $digits[$counter]. $plural.' '.$hundred:$words[floor($number / 10) * 10].' '.$words[$number % 10]. ' '.$digits[$counter].$plural.' '.$hundred;
-        } else $str[] = null;
-    }
-    $Rupees = implode('', array_reverse($str));
-    $paise = ($decimal > 0) ? "." . ($words[$decimal / 10] . " " . $words[$decimal % 10]) . ' Paise' : '';
-    return ($Rupees ? $Rupees . 'Rupees ' : '') . $paise;
-}
+// function getIndianCurrency(float $number) {
+//     $decimal = round($number - ($no = floor($number)), 2) * 100;
+//     $hundred = null;
+//     $digits_length = strlen($no);
+//     $i = 0;
+//     $str = array();
+//     $words = array(0 => '', 1 => 'one', 2 => 'two',
+//         3 => 'three', 4 => 'four', 5 => 'five', 6 => 'six',
+//         7 => 'seven', 8 => 'eight', 9 => 'nine',
+//         10 => 'ten', 11 => 'eleven', 12 => 'twelve',
+//         13 => 'thirteen', 14 => 'fourteen', 15 => 'fifteen',
+//         16 => 'sixteen', 17 => 'seventeen', 18 => 'eighteen',
+//         19 => 'nineteen', 20 => 'twenty', 30 => 'thirty',
+//         40 => 'forty', 50 => 'fifty', 60 => 'sixty',
+//         70 => 'seventy', 80 => 'eighty', 90 => 'ninety');
+//     $digits = array('', 'hundred','thousand','lakh', 'crore');
+//     while( $i < $digits_length ) {
+//         $divider = ($i == 2) ? 10 : 100;
+//         $number = floor($no % $divider);
+//         $no = floor($no / $divider);
+//         $i += $divider == 10 ? 1 : 2;
+//         if ($number) {
+//             $plural = (($counter = count($str)) && $number > 9) ? 's' : null;
+//             $hundred = ($counter == 1 && $str[0]) ? ' and ' : null;
+//             $str [] = ($number < 21) ? $words[$number].' '. $digits[$counter]. $plural.' '.$hundred:$words[floor($number / 10) * 10].' '.$words[$number % 10]. ' '.$digits[$counter].$plural.' '.$hundred;
+//         } else $str[] = null;
+//     }
+//     $Rupees = implode('', array_reverse($str));
+//     $paise = ($decimal > 0) ? "." . ($words[$decimal / 10] . " " . $words[$decimal % 10]) . ' Paise' : '';
+//     return ($Rupees ? $Rupees . 'Rupees ' : '') . $paise;
+// }
 
 ?>
